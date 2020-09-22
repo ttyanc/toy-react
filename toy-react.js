87e570e63121046223015d626db20faadafb5855
ttyanc <yantingtingchu@163.com>
2020-09-22 22:17:36 +0800

    lifecycle

--- toy-react.js
+++ toy-react.js
@@ -1,17 +1,34 @@
+const RENDER_TO_DOM = Symbol('render to dom')
 class ElementWrapper{
     constructor(type){
         this.root = document.createElement(type)
     }
     setAttribute(name,val){
-        this.root.setAttribute(name,val)
+        if(name.match(/^on([\s\S]+)$/)){
+            this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c=>c.toLowerCase()),val);
+        } else {
+            this.root.setAttribute(name,val)
+        }
     }
     appendChild(component){
-        this.root.appendChild(component.root)
+        let range = document.createRange();
+        range.setStart(this.root,this.root.childNodes.length);
+        range.setEnd(this.root,this.root.childNodes.length);
+        range.deleteContents();
+        component[RENDER_TO_DOM](range);
+    }
+    [RENDER_TO_DOM](range){
+        range.deleteContents();
+        range.insertNode(this.root)
     }
 }
 class TextWrapper{
     constructor(content){
         this.root = document.createTextNode(content)
+    }
+    [RENDER_TO_DOM](range){
+        range.deleteContents();
+        range.insertNode(this.root)
     }
 }
 
@@ -20,18 +37,40 @@ export class Component{
         this.props = Object.create(null);
         this.children = [];
         this._root = null;
+        this._range = null;
     }
     setAttribute(name,val){
         this.props[name] = val;
     }
+   
     appendChild(component){
         this.children.push(component)
     }
-    get root(){
-        if(!this._root){
-            this._root = this.render().root;
-        } 
-        return this._root
+    [RENDER_TO_DOM](range){
+        this._range = range;
+        this.render()[RENDER_TO_DOM](range)
+    }
+    rerender(){
+        this._range.deleteContents();
+        this[RENDER_TO_DOM](this._range)
+    }
+    setState(newState){
+        if(this.state === null || typeof this.state !== 'object'){
+            this.state = newState;
+            this.rerender()
+            return;
+        }
+        let merge = (oldState,newState) => {
+            for(let p in newState){
+                if(oldState[p] === null || typeof oldState[p] !== 'object'){
+                    oldState[p] = newState[p];
+                }else {
+                    merge( oldState[p],newState[p])
+                }
+            }
+        }
+        merge(this.state,newState)
+        this.rerender()
     }
 }
 
@@ -68,5 +107,9 @@ export function createElement (type, attrs, ...children){
 }
 
 export function render(component,parentElement){
-    parentElement.appendChild(component.root)
+    let range = document.createRange();
+    range.setStart(parentElement,0);
+    range.setEnd(parentElement,parentElement.childNodes.length);
+    range.deleteContents();
+    component[RENDER_TO_DOM](range);
 }
